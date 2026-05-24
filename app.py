5e41a7914aef2e911590786e58223d0c91d1255d
Pooja Sri A <poojasri30092001@gmail.com>
2026-05-24 12:44:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.set_page_config(page_title="AI Attention Dashboard", layout="wide")
+st.title("AI Classroom Attention Dashboard")
+st.metric("Attention Level","92%")
+st.metric("status","Attentive")
+st.metric("Head Position","Focused")
+st.success("System Running Successfully")
